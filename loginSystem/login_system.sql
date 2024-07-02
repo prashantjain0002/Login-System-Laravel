44cc389647889f71d9949b84aa7d2b5420ce0af2
Prashant Jain <prashantjain0002@gmail.com>
2024-07-02 19:36:14 +0530

    Add Sql File

--- loginSystem/login_system.sql
+++ loginSystem/login_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 02, 2024 at 12:36 PM
+-- Generation Time: Jul 02, 2024 at 04:02 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -72,7 +72,8 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('GtofTPNwd3HKduWNbU5E4BYQgSDTd2WELHU8sTI7', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36', 'YTozOntzOjk6Il9wcmV2aW91cyI7YToxOntzOjM6InVybCI7czoyNzoiaHR0cDovLzEyNy4wLjAuMTo4MDAwL2xvZ2luIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo2OiJfdG9rZW4iO3M6NDA6ImdTc01ZQUdINUZsd0xhQTF0RWV1S2xuYWhPb0hLQlZIS2lpclEwZ3AiO30=', 1719916242);
+('c3kTXXMQyz14ZenR1vzP4C6e2XPBAjhFkjGcx0aR', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36', 'YTozOntzOjk6Il9wcmV2aW91cyI7YToxOntzOjM6InVybCI7czoyMToiaHR0cDovLzEyNy4wLjAuMTo4MDAwIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo2OiJfdG9rZW4iO3M6NDA6ImlNSXk3WHhkbEdRWGpiM0xEQmU3c1hLNmFIdVpLcUNhNXJzOURPaDQiO30=', 1719919285),
+('UfRM6W9PLhcAWdfiH7ciLScGrPNgQaWywghVtrPy', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36', 'YTozOntzOjk6Il9wcmV2aW91cyI7YToxOntzOjM6InVybCI7czoyMToiaHR0cDovLzEyNy4wLjAuMTo4MDAwIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo2OiJfdG9rZW4iO3M6NDA6IjVPV1BBSzZIejRHR3htZlNRanJDaWdoTEl3ekFYVVJ6WUdRMTVTZWEiO30=', 1719928675);
 
 -- --------------------------------------------------------
 
@@ -98,7 +99,11 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
 (1, 'Prashant Jain', 'prashantjain8120@gmail.com', NULL, '$2y$12$w.pR/h0zzEwjwKnoishm7OhF2HFwHB9uLUmwMsYij79Zeo3kIQsJC', NULL, '2024-07-02 04:15:40', '2024-07-02 04:15:40'),
 (2, 'Ronak', 'ronak1002@gmail.com', NULL, '$2y$12$lN6pLyozryXNwsLsCA.RGev0EUKqSORcB8ZMsua8Fw23apfetfaxK', NULL, '2024-07-02 04:19:02', '2024-07-02 04:19:02'),
-(3, 'Harsh Jha', 'harshjha@gmail.com', NULL, '$2y$12$M.P.pd.YnxT3kj0qyhsS/O4FjIyzr.IsIduYaF.FrLxzCmJs5zZNW', NULL, '2024-07-02 04:45:59', '2024-07-02 04:45:59');
+(3, 'Harsh Jha', 'harshjha@gmail.com', NULL, '$2y$12$M.P.pd.YnxT3kj0qyhsS/O4FjIyzr.IsIduYaF.FrLxzCmJs5zZNW', NULL, '2024-07-02 04:45:59', '2024-07-02 04:45:59'),
+(5, 'Prashnat', 'prashantjain812023@gmail.com', NULL, '$2y$12$tBbufj1QFoARyqFZ25ceVeV46vw37ld74U807AJgZ0qnsdDy51iJW', NULL, '2024-07-02 05:48:43', '2024-07-02 05:48:43'),
+(6, 'Atul Purohit', 'atul@gmail.com', NULL, '$2y$12$w/O0MsR4ouUEu0HHpIVAFOhvGnJxhSOpqbqyhoHblDrh.kMST1PNu', NULL, '2024-07-02 05:49:42', '2024-07-02 05:49:42'),
+(7, 'Harsh Jha', 'harsh859@gmail.com', NULL, '$2y$12$eDQQXKR6MwLI9czZrv3RiOAaKJyQuEHYzXEtkRY5f3b0EIienYSHu', NULL, '2024-07-02 05:51:04', '2024-07-02 05:51:04'),
+(8, 'Hetu', 'Hetu123@gmail.com', NULL, '$2y$12$vwnGPKMIsMy4M9vvI9UDbu1HBuIkgIoR8Tfi.KbXO8OIfAlIxfAN2', NULL, '2024-07-02 08:27:30', '2024-07-02 08:27:30');
 
 --
 -- Indexes for dumped tables
@@ -145,7 +150,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
